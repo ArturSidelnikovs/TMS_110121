90dc614a63b3fb85d3933869da0dd74b03da63cd
ArturSidelnikovs <arturmatevosov1@gmail.com>
2021-01-22 02:22:05 +0300

    Lesson3: Homework 2

--- Lesson3Homework/src/com/company/Main.java
+++ Lesson3Homework/src/com/company/Main.java
@@ -8,9 +8,9 @@ D) недель + дней + часов + минут + секунд.
 по аналогии с примером выше.
  */
 public class Main {
-    public static void main(String[] args) {
-	      int s = 4500;
-	      int sec = s % 60;
+public static void main(String[] args) {
+	 int s = 4500;
+		 int sec = s % 60;
 				  int m = (s-sec)/60;
 				  int min = m % 60;
 				  int h = (m-min)/60;
@@ -19,13 +19,10 @@ public class Main {
 				  int days = d % 24;
 				  int w = (d - days)/24;
 				  int weekend = w % 24;
-						  System.out.println(min + " " + "минут"+ " " + sec + " " + "секунд" );
+				          System.out.println(min + " " + "минут"+ " " + sec + " " + "секунд" );
 		                  System.out.println(h +" " + "час"+ " " + min +" " + "минут" + " " + sec + " " + "секунд") ;
 		                  System.out.println(days+ " " + "дней "+ " " +hours+ " " + "часов"+ " " + min +" " + "минут" + " " + sec+ " " + "секунд") ;
 		                  System.out.println(weekend+" " + "недель "+ " " + days+" " + "дней "+ " " +hours+ " " + "час"+" " + min +" " + "минут" + " " + sec+ " " + "секунд") ;
-
-
-
 
     }
 }
