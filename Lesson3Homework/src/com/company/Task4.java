dca73ed245125748c2eb9aacead3ffcdcdc76b75
ArturSidelnikovs <arturmatevosov1@gmail.com>
2021-01-22 02:12:31 +0300

    Lesson3: Homework 1

---
+++ Lesson3Homework/src/com/company/Task4.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/*Задание 4
+*Определить число, полученное выписыванием в обратном порядке цифр
+*любого 4-х значного натурального числа n.
+* */
+
+public class Task4 {
+    public static void main(String[] args) {
+        int n = 3428;
+        char numberOne = 3;
+        char numberTwo = 4;
+        char numberThree = 2;
+        char numberFour = 8;
+        System.out.println("Число, записанное в обратном порядке 4х значного натурального числа n " +" " + numberFour+numberThree+numberTwo+numberOne );
+
+
+
+
+
+    }
+}
