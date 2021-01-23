94742619de0020049abeffaddc2e5bf9855aa8b6
ArturSidelnikovs <arturmatevosov1@gmail.com>
2021-01-23 18:25:09 +0300

    Lesson3: Task5

---
+++ Lesson3Homework/src/com/company/Task5.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/*Дано любое натуральное 4-х значное число. Верно ли, что все цифры числа
+* различны?
+*/
+
+public class Task5 {
+    public static void main(String[] args) {
+        int number = 1347;
+        int a = 1, b = 3, c = 4, d = 7;
+        System.out.println("a=b="+" "+(a==b)+" "+"a=c="+" "+(a==c)+" "+"a=d="+" "+(a==d));
+        System.out.println("b=c="+" "+ (b==c)+" "+"b=c="+" "+(b==c));
+        System.out.println("d=b="+" "+ (d==b)+" "+"d=c="+" "+(d==c));
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
