52a86c6b18c2966fa61e9f073861ab264304767d
ArturSidelnikovs <arturmatevosov1@gmail.com>
2021-01-25 02:13:32 +0300

    Lesson4: Task 11

---
+++ Lesson4Homework/src/Task11.java
@@ -0,0 +1,20 @@
+/*3) Дано целое число. Если оно является положительным,
+     то прибавить к нему 1.Если отрицательным, то вычесть из него 2.
+     Если нулевым, то заменить его на 10. Вывести полученное число
+ */
+
+public class Task11 {
+    public static void main(String[] args) {
+        int x = 103;
+        if (x >= 0) {
+            x++;
+            System.out.println(x);
+        } else if (x < 0) {
+            x = x-2;
+            System.out.println(x);
+        } else if (x==0) {
+            x=10;
+            System.out.println(x);
+        }
+    }
+}
