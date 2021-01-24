86fac08f297484f22c632eaa783bf2f52ae12036
ArturSidelnikovs <arturmatevosov1@gmail.com>
2021-01-25 02:23:52 +0300

    Lesson4: Task 11

---
+++ Lesson4Homework/src/Task12.java
@@ -0,0 +1,14 @@
+/*4) Даны 3 целых числа. Найти количество положительных чисел в исходном наборе.
+ */
+public class Task12 {
+    public static void main(String[] args) {
+        int x=2, y=4;
+        if (x>y) {
+            System.out.println("2  больше 4");
+        } else {
+            System.out.println("4 больше 2");
+
+        }
+
+    }
+}
