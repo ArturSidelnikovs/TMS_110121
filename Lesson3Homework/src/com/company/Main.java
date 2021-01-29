33fffb5c0f3627a16eb3af8b9fef59eb9afd83c7
ArturSidelnikovs <arturmatevosov1@gmail.com>
2021-01-30 01:34:47 +0300

    Task 3

--- Lesson3Homework/src/com/company/Main.java
+++ Lesson3Homework/src/com/company/Main.java
@@ -9,20 +9,19 @@ D) недель + дней + часов + минут + секунд.
  */
 public class Main {
 public static void main(String[] args) {
-	 int s = 4500;
+	 int s = 9129889;
 		 int sec = s % 60;
 				  int m = (s-sec)/60;
-				  int min = m % 60;
-				  int h = (m-min)/60;
-				  int hours = h % 60;
+	              int min = m % 60;
+	              int h = (m-min)/60;
+	              int hours = h % 24;
 				  int d = (h - hours)/24;
-				  int days = d % 24;
-				  int w = (d - days)/24;
-				  int weekend = w % 24;
+				  int days = d % 7;
+				  int w = (d - days)/7;
 				          System.out.println(min + " " + "минут"+ " " + sec + " " + "секунд" );
-		                  System.out.println(h +" " + "час"+ " " + min +" " + "минут" + " " + sec + " " + "секунд") ;
-		                  System.out.println(days+ " " + "дней "+ " " +hours+ " " + "часов"+ " " + min +" " + "минут" + " " + sec+ " " + "секунд") ;
-		                  System.out.println(weekend+" " + "недель "+ " " + days+" " + "дней "+ " " +hours+ " " + "час"+" " + min +" " + "минут" + " " + sec+ " " + "секунд") ;
+		                  System.out.println(h +" " + "час(ов)"+ " " + min +" " + "минут" + " " + sec + " " + "секунд") ;
+		                  System.out.println(days+ " " + "дня(ей) "+ " " +hours+ " " + "часов(ов)"+ " " + min +" " + "минут" + " " + sec+ " " + "секунд") ;
+		                  System.out.println(w+" " + "недель "+ " " + days+" " + "дней "+ " " +hours+ " " + "час(ов)"+" " + min +" " + "минут" + " " + sec+ " " + "секунд") ;
 
     }
 }
